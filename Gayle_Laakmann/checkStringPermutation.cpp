3a900fe3857ede2511b5025536f45a8b099229c2
Testingnine123 <testingnine123@gmail.com>
2016-02-03 00:31:38 -0500

    Book questions.

---
+++ Gayle_Laakmann/checkStringPermutation.cpp
@@ -0,0 +1,36 @@
+/*check if two strings are permuttion of each other
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+bool PermutationCheck (std::string &str1, std::string &str2)
+{
+	//sort the two strings
+	std::sort (str1.begin(), str1.end());
+	std::sort (str2.begin(), str2.end());
+
+	if (str1 == str2)
+		return true;
+	else
+		return false;
+}
+
+int main ()
+{
+	std::string str1, str2;
+	
+	std::cout << "Enter string 1: ";
+	getline (std::cin, str1);
+
+	std::cout << "Enter string 2: ";
+	getline (std::cin, str2);
+
+	if (PermutationCheck (str1, str2))
+		std::cout << "The two strings are permutation of each other!\n";
+	else
+		std::cout << "The two strings are NOT permutation of each other!\n";
+
+	return 0;
+}
