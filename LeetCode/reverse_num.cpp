34ceda315eb0987e52e22d7c0eb7c6cd1b153141
faizabidi <fabidi89@vt.edu>
2016-12-17 15:01:13 -0500

    Reverese a number.

---
+++ LeetCode/reverse_num.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cmath>
+#include <limits>
+
+class Solution{
+public:
+    int reverse(int x){
+    	// Convert int to string
+    	std::string str = std::to_string(x);
+
+    	// Find the size of the string    
+    	size_t length = str.size() - 1;  
+
+    	// String to store the reveresed number
+    	std::string rev_str;
+
+    	// To store the reversed number
+    	int rev_num_int;
+
+    	// Assuming no minus or plus sign in the input
+    	if(str[0] != '-' || str[0] != '+'){
+    		for(int i = length; i >=0; i--)
+    			rev_str.push_back(str[i]);
+    	}
+    	
+    	// Return 0 if out of range
+    	try{
+    		rev_num_int = std::stoi(rev_str);
+    	}
+    	catch (const std::out_of_range& oor){
+    		return 0;
+    		//std::cerr << "Out of Range error: " << oor.what() << '\n';
+    	}
+
+    	// If input is negative
+    	if(str[0] == '-')
+    		rev_num_int = rev_num_int * -1;
+    	return rev_num_int;
+	}		
+};
+
+int main(){
+	Solution obj1;
+	int num;
+
+	std::cout << "Enter a number:";
+	std::cin >> num;
+
+	std::cout << obj1.reverse(num) << std::endl;
+
+	return 0;
+}
