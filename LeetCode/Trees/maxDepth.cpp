c1b504d4649227497933af3615827b0b4b4f2f48
faizabidi <fabidi89@vt.edu>
2017-04-21 01:46:42 -0400

    Added better code.

--- LeetCode/Trees/maxDepth.cpp
+++ LeetCode/Trees/maxDepth.cpp
@@ -9,12 +9,40 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+ 
 class Solution {
 public:
+    // Recursive
     int maxDepth(TreeNode* root){
-        if(root == NULL)
+        if(!root)
             return 0;
-        return std::max(maxDepth(root->left), maxDepth(root->right)) + 1;
-        
+        if(root->left)
+            maxDepth(root->left);
+        if(root->right)
+            maxDepth(root->right);
+        return 1 + std::max(maxDepth(root->left), maxDepth(root->right));
+    }
+    
+    // Iterative
+    int maxDepth2(TreeNode* root){
+        int count = 0;
+        if(!root)
+            return count;
+        std::queue<TreeNode *> q;
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            count++;
+            while(size > 0){
+                TreeNode *temp = q.front();
+                q.pop();
+                if(temp->left)
+                    q.push(temp->left);
+                if(temp->right)
+                    q.push(temp->right);
+                size--;
+            }
+        }
+        return count;
     }
 };
