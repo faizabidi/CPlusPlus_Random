35a8986bcdfc99ecbae02faccd357c680cd60300
faizabidi <fabidi89@vt.edu>
2017-04-21 02:01:01 -0400

    Edited to better code.

--- LeetCode/Trees/checkSameTree.cpp
+++ LeetCode/Trees/checkSameTree.cpp
@@ -13,62 +13,53 @@ class Solution {
 public:
     // Non-recursive solution
     bool isSameTree(TreeNode* p, TreeNode* q){
-        // If only any one of them is NULL
-        if(!p && q || p && !q)
-            return false;
-        // If both are NULL
         if(!p && !q)
             return true;
-        if(p->val != q->val)
+        if(!p)
             return false;
-        // Apply Level Order Traversal
+        if(!q)
+            return false;
         std::queue<TreeNode *> q1, q2;
-        q1.push(p); q2.push(q);
+        q1.push(p);
+        q2.push(q);
         while(!q1.empty() && !q2.empty()){
-            TreeNode *q1_temp = q1.front();
-            TreeNode *q2_temp = q2.front();
-            q1.pop(); q2.pop();
-            // Check if values are same
-            if(q1_temp->val != q2_temp->val)
+            TreeNode *temp1, *temp2;
+            temp1 = q1.front();
+            temp2 = q2.front();
+            q1.pop();
+            q2.pop();
+            if(temp1->val != temp2->val)
                 return false;
-            // If left sub-tree not empty
-            if(q1_temp->left && q2_temp->left){
-                q1.push(q1_temp->left);
-                q2.push(q2_temp->left);
-            }
-            // If one left child is empty and the other is not
-            if(!q1_temp->left && q2_temp->left)
+            if(temp1->left)
+                q1.push(temp1->left);
+            if(temp2->left)
+                q2.push(temp2->left);
+            if((temp1->left && !temp2->left) || (!temp1->left && temp2->left))
                 return false;
-            if(q1_temp->left && !q2_temp->left)
-                return false;
-            // If right sub-tree not empty
-            if(q1_temp->right && q2_temp->right){
-                q1.push(q1_temp->right);
-                q2.push(q2_temp->right);
-            }
-            // If one right child is empty and the other is not
-            if(!q1_temp->right && q2_temp->right)
-                return false;
-            if(q1_temp->right && !q2_temp->right)
+            if(temp1->right)
+                q1.push(temp1->right);
+            if(temp2->right)
+                q2.push(temp2->right);
+            if((temp1->right && !temp2->right) || (!temp1->right && temp2->right))
                 return false;
         }
         return true;
-        // Recursive solution
-        bool isSameTree(TreeNode* p, TreeNode* q){
-            // If both empty
-            if(!p && !q)
-                return true;
-            // If both are non-empty
-            if(p && q){
-                return
-                (
-                    (p->val == q->val) &&
-                    isSameTree(p->left, q->left) &&
-                    isSameTree(p->right, q->right)
-                );
-            }
-            // One is empty and other is not
-            return false;
+    }
+    // Recursive solution
+    bool isSameTree(TreeNode* p, TreeNode* q){
+        // If both empty
+        if(!p && !q)
+            return true;
+        // If both are non-empty
+        if(p && q){
+            return
+            (
+                (p->val == q->val) &&
+                isSameTree(p->left, q->left) &&
+                isSameTree(p->right, q->right)
+            );
         }
+        // One is empty and other is not
+        return false;
     }
 };
