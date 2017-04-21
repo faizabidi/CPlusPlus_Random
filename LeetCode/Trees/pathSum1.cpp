12e8f433f74cf1003cb409a9ec33b5f341fe6571
faizabidi <fabidi89@vt.edu>
2017-04-21 02:20:14 -0400

    Edited code to make it better.

--- LeetCode/Trees/pathSum1.cpp
+++ LeetCode/Trees/pathSum1.cpp
@@ -11,31 +11,15 @@
  */
 class Solution {
 public:
+    class Solution {
+public:
     bool hasPathSum(TreeNode* root, int sum){
-        std::string path;
-        if(!root){
-            std::cout << path;
+        if(!root)
             return false;
-        }
-        path = std::to_string(root->val);
-        if(hasPathSum_helper(root, path, sum))
+        // If leaf found
+        if(!root->left && !root->right && sum == root->val)
             return true;
-        return false;
-    }
-    
-    bool hasPathSum_helper(TreeNode* temp, std::string path, int sum){
-        bool flag = false;
-        // If leaf and found
-        if(!temp->left && !temp->right && temp->val == sum){
-            std::cout << "Value of this leaf is " << temp->val << std::endl;
-            std::cout << path << std::endl;
-            return true;
-        }
-        int new_sum = sum - temp->val;
-        if(temp->left)
-            flag = flag || hasPathSum_helper(temp->left, path + "->" + std::to_string(temp->left->val), new_sum);
-        if(temp->right)
-            flag = flag || hasPathSum_helper(temp->right, path + "->" + std::to_string(temp->right->val), new_sum);
-        return flag;
+        // Else, go left and right
+        return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val);
     }
 };
