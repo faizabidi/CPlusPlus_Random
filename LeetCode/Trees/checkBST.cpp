18e45bb2074fbb2a119009f5e23ad2f6b509e406
faizabidi <fabidi89@vt.edu>
2017-04-22 19:35:10 -0400

    Removed a comment line.

--- LeetCode/Trees/checkBST.cpp
+++ LeetCode/Trees/checkBST.cpp
@@ -15,15 +15,15 @@ public:
     bool isValidBST(TreeNode *root){
         return isValidBST_Helper(root, LONG_MIN, LONG_MAX);
     }
+    
     bool isValidBST_Helper(TreeNode* root, long min, long max){
         // If root is NULL
         if(!root)
             return true;
         // If root is not NULL
         if(root->val > min && root->val < max){
-                //if(root->val >= root->left->val && root->val < root->right->val)
-                return isValidBST_Helper(root->left, min, root->val) 
-                        && isValidBST_Helper(root->right, root->val, max);
+            return isValidBST_Helper(root->left, min, root->val) 
+                    && isValidBST_Helper(root->right, root->val, max);
         }
         return false;
     }
