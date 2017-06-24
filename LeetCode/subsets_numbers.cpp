4595f7f441c6a758c408dab3e3c86d07e08305bf
faizabidi <fabidi89@vt.edu>
2017-06-24 17:45:52 -0400

    Added a non-bit method.

--- LeetCode/subsets_numbers.cpp
+++ LeetCode/subsets_numbers.cpp
@@ -4,16 +4,24 @@
 #include <vector>
 #include <cmath>
 
-class Solution {
+class Solution{
 public:
 	void printVector(std::vector<std::vector<int>> array){
 		for(int i = 0; i < array.size(); i++){
+			std::cout << "{";
 			for(int j = 0; j < array[i].size(); j++)
-				std::cout << array[i][j] << " ";
-			std::cout << std::endl;
+				std::cout << array[i][j];
+			std::cout << "}" << std::endl;
 		}
 	}
 
+	void printVector2(std::vector<int> array){
+		for(int i = 0; i < array.size(); i++)
+			std::cout << array[i] << " ";
+		std::cout << std::endl;
+	}
+
+	// Bit manipulation
     std::vector<std::vector<int>> subsets(std::vector<int> nums){
     	std::vector<std::vector<int>> ans;
     	// Total number of distinct combinations = 2^N
@@ -29,13 +37,32 @@ public:
     	}
     	return ans;
     }
+
+    // Without bit-manipulation
+    // REF: https://discuss.leetcode.com/topic/19110/c-recursive-iterative-bit-manipulation-solutions-with-explanations
+    std::vector<std::vector<int>> subsets2(std::vector<int> nums){
+    	std::vector<std::vector<int>> ans(1, std::vector<int>());
+
+    	for(int i = 0; i < nums.size(); i++){
+    		int n = ans.size();
+    		// j will keep the count of vector in ans
+    		for(int j = 0; j < n; j++){
+    			// Push the vector
+    			ans.push_back(ans[j]);
+    			// Push the number
+    			std::vector<int> temp = ans.back();
+    			ans.back().push_back(nums[i]);
+    		}
+    	}
+    	return ans;
+    }
 };
 
 int main(){
 	std::vector<int> nums = {1,2,3};
 
 	Solution obj1;
-	std::vector<std::vector<int>> subsets = obj1.subsets(nums);
+	std::vector<std::vector<int>> subsets = obj1.subsets2(nums);
 	obj1.printVector(subsets);
 
 	return 0;
