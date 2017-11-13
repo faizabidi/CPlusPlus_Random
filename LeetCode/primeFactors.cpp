45773bab9c20b9be66faab9c1859b65709caad3a
faizabidi <fabidi89@vt.edu>
2017-11-12 21:40:37 -0500

    Minor changes.

--- LeetCode/primeFactors.cpp
+++ LeetCode/primeFactors.cpp
@@ -2,18 +2,18 @@
 #include <vector>
 #include <cmath>
 
-bool isPrime(long long n){
+bool isPrime(int n){
 	if(n == 2)
 		return true;
-	for(long long i = 2 ; i <= sqrt(n); i++){
+	for(int i = 2 ; i <= sqrt(n); i++){
 		if(n % i == 0)
 			return false;
 	}
 	return true;
 }
 
-void printVector(std::vector<long long> array){
-	for(long long i = 0; i < array.size(); i++){
+void printVector(std::vector<int> array){
+	for(int i = 0; i < array.size(); i++){
 		if(i != array.size() - 1) 
 			std::cout << array[i] << " x ";
 		else
@@ -22,18 +22,22 @@ void printVector(std::vector<long long> array){
 	std::cout << std::endl;
 }
 
-void findPrimeFactors(long long n){
-	std::vector<long long> factors;
+std::vector<int> findPrimeFactors(int n){
+	std::vector<int> factors;
+	if(n == 1){
+		factors.push_back(n);
+		return factors;
+	}
 	while(n > 1){
 		// If n is prime
 		if(isPrime(n)){
 			factors.push_back(n);
-			break;
+			return factors;
 		}
 		else{
 			// Find the smallest prime number that fully divides n
 			// Start with 2 and keep doing this till we get 0 or 1 in the end
-			long long primeNum = 2;
+			int primeNum = 2;
 			while(primeNum < n){
 				if(isPrime(primeNum) && n % primeNum == 0){
 					factors.push_back(primeNum);
@@ -45,14 +49,15 @@ void findPrimeFactors(long long n){
 			}		
 		}
 	}
-	printVector(factors);
+	return factors;
 }
 
 int main(){
-	long long n;
+	int n;
 	std::cin >> n;
 
 	std::cout << "Prime factors are: ";
-	findPrimeFactors(n);
+	std::vector<int> factor = findPrimeFactors(n);
+	printVector(factor);
 	return 0;
 }
