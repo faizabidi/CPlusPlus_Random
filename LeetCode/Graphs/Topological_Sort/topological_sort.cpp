e7fcf227cbb025481e7e3da541439979f9197c8a
Faiz Abidi <fabidi89@vt.edu>
2018-04-21 13:49:54 -0400

    Minor changes.

--- LeetCode/Graphs/Topological_Sort/topological_sort.cpp
+++ LeetCode/Graphs/Topological_Sort/topological_sort.cpp
@@ -1,12 +1,11 @@
+// http://www.techiedelight.com/topological-sorting-dag/
+
 #include <iostream>
 #include <vector>
 #include <list>
-#include <stack>
 
-void printVector(std::vector<int> arrray){
-	for(int i = 0; i < arrray.size(); i++)
-		std::cout << i << ":" << arrray[i] << " ";
-	std::cout << std::endl;
+bool compare(std::pair<int, int> a, std::pair<int, int> b){
+	return (a.second > b.second);
 }
 
 struct Edge{
@@ -29,36 +28,32 @@ void printGraph(std::vector<std::list<int>> adjList, int N){
 		for(it = adjList[i].begin(); it != adjList[i].end(); it++)
 			std::cout << *it << "->";
 		std::cout << "NULL\n";
-	}
+	} 
 }
 
 // Do a DFS traversal recursively
 void DFS_recursion(std::vector<std::list<int>> adjList, 
-							std::vector<bool> &visited, std::stack<int> &s, 
-							std::vector<int> &arrival_time, 
+		std::vector<bool> &visited, int start, std::vector<int> &arrival_time, 
 							std::vector<int> &departure_time, int &time){
-	
-	if(s.empty())
-		return;
 
-	int start = s.top();
-	s.pop();
 	arrival_time[start] = time++;
+
+	std::cout << start << " ";
+	visited[start] = true;
 
 	// Add all its vertices to the stack
 	std::list<int>::iterator it;
 	for(it = adjList[start].begin(); it != adjList[start].end(); it++){
 		if(!visited[*it]){
-			s.push(*it);
 			visited[*it] = true;
-			DFS_recursion(adjList, visited, s, arrival_time, departure_time, time);
+			DFS_recursion(adjList, visited, *it, arrival_time, departure_time, time);
 		}
 	}
 	departure_time[start] = time++;
 }
 
 int main(){
-	std::vector<Edge> edges1 = {
+	std::vector<Edge> edges = {
 		{0, 6},
 		{1, 2},
 		{1, 4},
@@ -69,19 +64,6 @@ int main(){
 		{7, 0},
 		{7, 1}
 	};
-
-	std::vector<Edge> edges = {
-		{0, 1},
-		{0, 2},
-		{2, 3},
-		{2, 4},
-		{3, 1},
-		{3, 5},
-		{4, 5},
-		{6, 7}
-	};
-
-
 
 	int N = 8;
 
@@ -95,14 +77,28 @@ int main(){
 
 	// DFS search using recursive method
 	std::vector<bool> visited(N, false);
-	std::stack<int> s;
-	s.push(0);
 	std::vector<int> arrival_time(N, 0), departure_time(N, 0);
 	int time = 0;
-	DFS_recursion(adjList, visited, s, arrival_time, departure_time, time);
+	// DFS from all the nodes so that even un-connected nodes get visited
+	for(int i = 0; i < N; i++){
+		if(!visited[i])
+			DFS_recursion(adjList, visited, i, arrival_time, departure_time, time);
+	}
+	std::cout << "Arrival/departure time of nodes is as follows\n";
+	for(int i = 0; i < N; i++)
+		std::cout << "Node" << i << ":" << arrival_time[i] << "," << departure_time[i] << std::endl;
+
+	// Print nodes in decreasing order of their departure times
+	// That will be the topological sort. 
+	std::cout << "Topological sort is\n"; // Ans = 7,5,3,1,4,2,0,6
+	std::vector<std::pair<int, int>> array(N);
+	for(int i = 0; i < N; i++)
+		array[i] = std::make_pair(i, departure_time[i]);
+	// Sort this vector in decreasing order of the second int in the pair
+	std::sort(array.begin(), array.end(), compare); 
+	for(int i = 0; i < array.size(); i++)
+		std::cout << array[i].first << " ";
 	std::cout << std::endl;
-	printVector(arrival_time);
-	printVector(departure_time);
 
 	return 0;
 }
