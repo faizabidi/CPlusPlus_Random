c8d2dbfc518eb00466ef9476d1ea0347cd670584
Faiz Abidi <fabidi89@vt.edu>
2018-05-12 15:03:51 -0400

    Permute string for lower and upper case.

---
+++ LeetCode/Strings/permute_upperLowercase.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/letter-case-permutation/description/
+
+class Solution {
+public:
+    void letterCasePermutation_helper(string S, int index, 
+        vector<string> &ans, string temp){
+
+        if(index == S.size()){
+            if(find(ans.begin(), ans.end(), temp) == ans.end())
+                ans.push_back(temp);
+            return;
+        }
+        // Make uppercase
+        letterCasePermutation_helper(S, index+1, ans, 
+                                temp + char(toupper(S[index])));
+
+        // Make lowercase
+        letterCasePermutation_helper(S, index+1, ans, 
+                                temp + char(tolower(S[index])));
+        
+    }
+    vector<string> letterCasePermutation(string S){
+        vector<string> ans;
+        string temp = "";
+        letterCasePermutation_helper(S, 0, ans, temp);
+        return ans;
+    }
+};
