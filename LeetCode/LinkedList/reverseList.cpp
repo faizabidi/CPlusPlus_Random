191064b9282d70b244b23929e911cb5946e020a4
Faiz Abidi <fabidi89@vt.edu>
2018-06-16 15:58:07 -0400

    Added a recursive solution.

--- LeetCode/LinkedList/reverseList.cpp
+++ LeetCode/LinkedList/reverseList.cpp
@@ -10,6 +10,8 @@
  */
 class Solution {
 public:
+    // Iteratively
+    // O(n) time & O(1) space
     ListNode* reverseList(ListNode* head){
         ListNode *prev = NULL, *curr = head;
         while(curr != NULL){
@@ -20,5 +22,18 @@ public:
         }
         head = prev;
         return head;
+    }
+    // Recursively
+    // O(n) time & O(n) space
+    // https://stackoverflow.com/questions/354875/reversing-a-linked-list-in-java-recursively
+    ListNode* reverseList(ListNode* head){
+        if(!head || !head->next)
+            return head;
+        
+        ListNode *temp = head->next;
+        head->next = NULL;
+        ListNode *nextElem = reverseList(temp);
+        temp->next = head;
+        return nextElem;
     }
 };
