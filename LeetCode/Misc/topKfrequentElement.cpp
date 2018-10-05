827a9e1231c7289d0d03ef893b2f700f29672d3c
faizabidi <fabidi89@vt.edu>
2018-10-05 01:43:47 -0400

    Added O(n) time function.

--- LeetCode/Misc/topKfrequentElement.cpp
+++ LeetCode/Misc/topKfrequentElement.cpp
@@ -2,6 +2,7 @@
 
 class Solution {
 public:
+    // O(nlogn) time and O(n) space
     // Sort in desc order based on second element
     static bool sortDesc(pair<int, int> a, pair<int, int> b){
         //if(a.second > b.second)
@@ -27,6 +28,33 @@ public:
         // Print the k elements
         for(int i = 0; i < k && i < sequenceOrder.size(); i++)
             ans.push_back(sequenceOrder[i].first);
+        return ans;
+    }
+
+    // O(n) time and O(n) space
+    vector<int> topKFrequent(vector<int>& nums, int k){
+        map<int, int> hashmap;
+        int maxSize = 0;
+        vector<int> ans;
+        for(auto value : nums)
+            hashmap[value]++;
+        // Bucket sort the hashmap
+        vector<vector<int>> sortedArrayFreq(nums.size() + 1);
+        for(auto it = hashmap.begin(); it != hashmap.end(); it++)
+            sortedArrayFreq[it->second].push_back(it->first);
+        // We need to build a vector of size k from this sortedArrayFreq
+        for(int i = sortedArrayFreq.size() - 1; i >= 0; i--){
+            if(ans.size() == k)
+                break;
+            vector<int> temp = sortedArrayFreq[i];
+            if(temp.size() > 0){
+                int j = 0;
+                while(ans.size() < k && j < temp.size()){
+                    ans.push_back(temp[j]);
+                    j++;
+                }
+            }
+        }
         return ans;
     }
 };
