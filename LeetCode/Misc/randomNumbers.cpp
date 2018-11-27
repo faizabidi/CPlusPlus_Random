f6226fa033251565b6e58c086da69b60159829e9
faizabidi <fabidi89@vt.edu>
2018-11-27 17:11:08 -0500

    Generate random numbers.

--- LeetCode/Misc/randomNumbers.cpp
+++ LeetCode/Misc/randomNumbers.cpp
@@ -6,14 +6,14 @@ using namespace std;
 // Generate random numbers between low and high
 void generateNumbers(int low, int high){
     for(int i = low; i <= high; i++){
-        cout << rand() % 10 << endl;
+        cout << (rand() % (high - low + 1)) + low << endl;
     }
 }
 
 int main(){
     // Seed with time to get different random numbers on each run
     srand(time(NULL));
-    generateNumbers(0, 10);
+    generateNumbers(95, 100);
 
     return 0;
 }
