cd5938a9df95621a63407b15475a82106afd6255
faizabidi <fabidi89@vt.edu>
2019-02-06 14:16:34 -0500

    Added brute-force solution.

--- LeetCode/Dynamic_Programming/Stairs/stairs.cpp
+++ LeetCode/Dynamic_Programming/Stairs/stairs.cpp
@@ -33,3 +33,18 @@ public:
         return ways;
     }
 };
+
+// Recursive, bruteforce solution
+// O(2^n) time
+class Solution {
+public:
+    int climbStairs(int n){
+        if(n <= 1)
+            return 1;
+        if(n == 2)
+            return 2;
+        int oneStep = climbStairs(n-1);
+        int twoSteps = climbStairs(n-2);
+        return oneStep+twoSteps;
+    }
+};
