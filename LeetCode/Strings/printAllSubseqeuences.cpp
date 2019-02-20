a68a8a3b4d0dc69d7fb5e20bedafb09dfa7093f0
faizabidi <fabidi89@vt.edu>
2019-02-20 15:23:16 -0500

    modified a bit.

--- LeetCode/Strings/printAllSubseqeuences.cpp
+++ LeetCode/Strings/printAllSubseqeuences.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 using namespace std;
 
+// O(2^n) time
 void printAllSubsequences(string str1, string temp, int index){
     if(index == str1.size()){
         cout << temp << endl;
@@ -9,15 +10,10 @@ void printAllSubsequences(string str1, string temp, int index){
     }
 
     // Include it
-    string temp2 = temp + str1[index];
-    //cout << temp2 << endl;
-    //return;
-    printAllSubsequences(str1, temp2, index+1);
+    printAllSubsequences(str1, temp+str1[index], index+1);
     // Exclude it
     printAllSubsequences(str1, temp, index+1);
-
 }
-
 
 int main(){
     string str1 = "ABC";
